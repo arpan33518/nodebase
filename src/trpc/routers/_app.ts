8896fbba291921e534b6785f574bb17d482a4c32
bitbypan <arpanpurkaitprof@gmail.com>
2026-03-30 08:52:24 +0530

    11-workflows-crud

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,36 +1,11 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
-import { prisma } from '@/lib/prisma';
-import { inngest } from '@/inngest/client';
-import { google } from '@ai-sdk/google';
-import { generateText } from 'ai';
-import { TRPCError } from '@trpc/server';
+import { createTRPCRouter } from '../init';
+import { workflowsRouter } from '@/features/workflows/server/routers';
 
 
 export const appRouter = createTRPCRouter({
-
-  testAi: baseProcedure.mutation(async () => {
-
-    // throw new TRPCError({
-    //   code: "BAD_REQUEST", message: "something went wrong"
-    // });
-    await inngest.send({
-      name: "execute/ai",
-    });
-    return "AI execution triggered";
-  }),
-  getWorkflows: baseProcedure.query(() => {
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "arpan@gmail.com",
-      }
-    })
-  })
+workflows:workflowsRouter,
+ 
 });
 
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
