4c7920c5f52d47ed611ca2bdc6394026cfb061bf
arpan33518 <arpanpurkaitprof@gmail.com>
2026-04-28 13:32:02 +0530

    19:node templating

--- src/features/executions/components/lib/executor-registry.ts
+++ src/features/executions/components/lib/executor-registry.ts
@@ -3,13 +3,15 @@ import { NodeExecutor } from "../../types";
 import { manualTriggerExecutor } from "@/features/triggers/components/manual-trigger/executor";
 import { httpRequestExecutor } from "@/features/executions/components/http-request/executor";
 
-export const executorRegistry: Record<NodeType, NodeExecutor> = {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const executorRegistry: Record<NodeType, NodeExecutor<any>> = {
+
   [NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
   [NodeType.INITIAL]: manualTriggerExecutor,
   [NodeType.HTTP_REQUEST]: httpRequestExecutor,
 };
 
-export const getExecutor = (type: NodeType): NodeExecutor => {
+export const getExecutor = (type: NodeType): NodeExecutor<any> => {
   const executor = executorRegistry[type];
   if (!executor) {
     throw new Error(`No executor found for node type: ${type}`);
